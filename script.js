a085f6a86934e253beb385b8fa280d5e2084980a
vitor-mat <vitormateusalves123@gmail.com>
2021-12-15 15:14:16 -0300

    validacao de verificar se o input estar vazio, se os numero nao sao inteiro, se sao negativos e se o maximo e menor que o minimo feitas

--- script.js
+++ script.js
@@ -5,10 +5,35 @@ function draw(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function validationFunction(min, max){
+
+    if(!min && min!==0) return "Número mínimo não foi informado!";
+
+    if(!max && max!==0) return "Número máximo não foi informado!";
+
+    if(!Number.isInteger(min)) return "O número mínimo é invalido"
+
+    if(!Number.isInteger(max)) return "O número máximo é invalido"
+
+    if(min < 0) return "O número mínimo não pode ser menor que zero!"
+
+    if(max < 0) return "O número máximo não pode ser menor que zero!"
+
+    if(Number(max) < Number(min)) return "O número mínimo é maior que o máximo!";
+
+    return false;
+}
+
 document.querySelector("#drawn-btn").addEventListener("click", () => {
-    const minNumber = document.querySelector("#min-number").value
-    const maxNumber = document.querySelector("#max-number").value
-    
+    const minNumber = Number(document.querySelector("#min-number").value)
+    const maxNumber = Number(document.querySelector("#max-number").value)
+
+    const validationValue = validationFunction(minNumber, maxNumber)
+
+    if(validationValue){
+        return alert("Error: " + validationValue);
+    }
+
     const drawnNumber = draw(minNumber, maxNumber)
 
     const displayNumber = document.querySelector("#display-number");
